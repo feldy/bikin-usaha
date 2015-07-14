9fbbcad2ef302445879762c0860f1456a2b9d516
Feldy Yusuf <f3ldy10rpl@gmail.com>
2015-07-14 16:20:09 +0700

    impelemtn jenis usana baru

--- form/system/jenis_usaha_baru_service.php
+++ form/system/jenis_usaha_baru_service.php
@@ -14,8 +14,8 @@
 		$file_photo3 = $_FILES['file_photo3']['name'];
 		$file_photo4 = $_FILES['file_photo4']['name'];
 		$file_doc = $_FILES['file_doc']['name'];
-		$deskripsi = $_POST['deskripsi'];
-
+		$deskripsi = addslashes($_POST['deskripsi']);
+		// echo "deskripsi".$deskripsi;
 		$ext1 = pathinfo($file_photo1, PATHINFO_EXTENSION);
 		$ext2 = pathinfo($file_photo2, PATHINFO_EXTENSION);
 		$ext3 = pathinfo($file_photo3, PATHINFO_EXTENSION);
@@ -28,15 +28,14 @@
 		$fileName4 = $id."_file4.".$ext4;
 		$fileName5 = $id."_file_doc.".$ext5;
 
-		move_uploaded_file($_FILES['file_photo1']['tmp_name'], '../upload/images/'.$fileName1);
-		move_uploaded_file($_FILES['file_photo2']['tmp_name'], '../upload/images/'.$fileName2);
-		move_uploaded_file($_FILES['file_photo3']['tmp_name'], '../upload/images/'.$fileName3);
-		move_uploaded_file($_FILES['file_photo4']['tmp_name'], '../upload/images/'.$fileName4);
-		move_uploaded_file($_FILES['file_doc']['tmp_name'], '../upload/document/'.$fileName5);
 
-		$result = mysql_query("INSERT INTO m_jenis_usaha VALUES('$id', '$nama_usaha', '$no_tlp', '$jenis_usaha', $modal, $jumlah_pegawai, '$deskripsi', '$fileName1', '$fileName2', '$fileName3', '$fileName4', '$fileName5', 0) ") or die(mysql_error());
-
+		$result = mysql_query("INSERT INTO m_jenis_usaha VALUES('$id', '$nama_usaha', '$no_tlp', '$jenis_usaha', $modal, $jumlah_pegawai, '$deskripsi', '$fileName1', '$fileName2', '$fileName3', '$fileName4', '$fileName5', now(),  0) ") or die(mysql_error());
 		if ($result) {
+				move_uploaded_file($_FILES['file_photo1']['tmp_name'], '../upload/images/'.$fileName1);
+				move_uploaded_file($_FILES['file_photo2']['tmp_name'], '../upload/images/'.$fileName2);
+				move_uploaded_file($_FILES['file_photo3']['tmp_name'], '../upload/images/'.$fileName3);
+				move_uploaded_file($_FILES['file_photo4']['tmp_name'], '../upload/images/'.$fileName4);
+				move_uploaded_file($_FILES['file_doc']['tmp_name'], '../upload/document/'.$fileName5);
 				echo "<script> alert('Terima Kasih Partisipasinya !! Admin pengelola akan me-review pengajuan anda.'); window.location.href='../index.php'</script>";
 			} else {
 				echo "<script> alert('Proses Gagal Silahkan Ulangi !'); window.history.back();</script>";
